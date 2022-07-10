906a6df6399ba954c07728a9b907b90a2e48670d
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-10 10:09:51 +0800

    Create user.ts

---
+++ src/commands/user.ts
@@ -0,0 +1,23 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { SubCommandPluginCommand, SubCommandPluginCommandOptions } from '@sapphire/plugin-subcommands';
+import type { Message } from 'discord.js';
+import { registerUser } from '../services/userService';
+
+@ApplyOptions<SubCommandPluginCommandOptions>({
+    description: 'A user command',
+    subCommands: [
+        'register',
+    ],
+})
+export class UserCommand extends SubCommandPluginCommand {
+
+    public async register(message: Message) {
+        let isUserRegistered = await registerUser(message.author.id);
+
+        if (!isUserRegistered) {
+            return message.channel.send('You are already registered!');
+        }
+
+        return message.channel.send('You have registered!');
+    }
+}
