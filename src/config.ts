a8651a0ac36ace4d72c80eee08a7db2508d11c6d
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-10 10:09:47 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,12 @@
 export const BOT_TOKEN = process.env.BOT_TOKEN || '';
 export const BOT_PREFIX = '!';
 
+export const API_URL = process.env.API_URL || '';
+export const API_KEY = process.env.API_KEY || '';
+
 export default {
     BOT_TOKEN,
     BOT_PREFIX,
+    API_URL,
+    API_KEY,
 }
