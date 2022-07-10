c99d478645537d3a949355858c890ab35f1275c2
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-10 10:09:39 +0800

    Create enums.ts

---
+++ src/enums.ts
@@ -0,0 +1,13 @@
+export enum AttributeEnum {
+    HP = 1,
+    Strength = 2,
+    Defense = 3,
+    CriticalChance = 4,
+    CriticalDamage = 5,
+    EvadeChance = 6,
+    EscapeChance = 7,
+}
+
+export enum LocationEnum {
+    Hometown = 1,
+}
