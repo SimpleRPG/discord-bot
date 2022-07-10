d110df2f09e3972d698ff83fb556acb26a3d6596
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-10 21:18:51 +0800

    Update userService.ts

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -4,17 +4,11 @@ import type { definitions } from "../types/supabase";
 
 export const registerUser = async (discordId: string): Promise<boolean> => {
     // check if character with discord id matches a character in the database
-    let res = await supabase
-        .from<definitions['characters']>('characters')
-        .select('*')
-        .eq('discord_id', discordId);
-
-    if (res.body![0]) {
-        // if character exists, return false
+    if (await checkCharacterExists(discordId)) {
         return false;
     }
 
-    const { body: characters } = await supabase
+    let { body: character } = await supabase
         .from<definitions['characters']>('characters')
         .insert({
             discord_id: discordId,
@@ -22,50 +16,47 @@ export const registerUser = async (discordId: string): Promise<boolean> => {
             exp: 0,
             money: 200,
             location_id: LocationEnum.Hometown,
-        });
+        })
+        .single();
 
-    const character = characters![0];
-
-    // get all attributes from the database
-    const { body: attributes } = await supabase
-        .from<definitions['attributes']>('attributes')
-        .select();
-
-    // insert attributes with values into character_attributes
-    attributes?.forEach(async (attribute) => {
-        let value = 0;
-
-        switch (attribute.id) {
-            case AttributeEnum.HP:
-                value = 50;
-                break;
-            case AttributeEnum.Strength:
-                value = 15;
-                break;
-            case AttributeEnum.Defense:
-                value = 5;
-                break;
-            case AttributeEnum.CriticalChance:
-                value = 0;
-                break;
-            case AttributeEnum.CriticalDamage:
-                value = 0;
-                break;
-            case AttributeEnum.EvadeChance:
-                value = 0;
-                break;
-            case AttributeEnum.EscapeChance:
-                value = 0;
-        }
-
-        await supabase
-            .from<definitions['character_attributes']>('character_attributes')
-            .insert({
-                character_id: character.id,
-                attribute_id: attribute.id,
-                value: value,
-            });
-    });
+    await supabase.from<definitions['character_attributes']>('character_attributes')
+        .insert([
+            {
+                attribute_id: AttributeEnum.HP,
+                character_id: character!.id,
+                value: 50,
+            },
+            {
+                attribute_id: AttributeEnum.Strength,
+                character_id: character!.id,
+                value: 15,
+            },
+            {
+                attribute_id: AttributeEnum.Defense,
+                character_id: character!.id,
+                value: 5,
+            },
+            {
+                attribute_id: AttributeEnum.CriticalChance,
+                character_id: character!.id,
+                value: 0,
+            },
+            {
+                attribute_id: AttributeEnum.CriticalDamage,
+                character_id: character!.id,
+                value: 0,
+            },
+            {
+                attribute_id: AttributeEnum.EvadeChance,
+                character_id: character!.id,
+                value: 0,
+            },
+            {
+                attribute_id: AttributeEnum.EscapeChance,
+                character_id: character!.id,
+                value: 0,
+            },
+        ]);
 
     return true;
 }
@@ -79,17 +70,15 @@ export const getCharacterCurrentLocation = async (character: definitions['charac
     return locations![0];
 }
 
-export const checkCharacterExists = async (discordId: string): Promise<boolean> => {
-    return supabase
+export const checkCharacterExists = async (discordId: string) => {
+    const res = await supabase
         .from<definitions['characters']>('characters')
-        .select()
-        .eq('discord_id', discordId)
-        .then((res) => {
-            if (res.body![0]) {
-                return true;
-            }
-            return false;
-        });
+        .select('discord_id')
+        .eq('discord_id', discordId);
+    if (res.body![0]) {
+        return true;
+    }
+    return false;
 }
 
 export const attributeValueToString = (value: number, isPercentage: boolean): string => {
