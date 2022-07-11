eb353c2eb21c2c2686195429b4598d25d48eee51
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-12 04:35:53 +0800

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,7 +1,18 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient, SupabaseClientOptions } from '@supabase/supabase-js'
 import config from './config';
 
+const options = <SupabaseClientOptions>{
+    persistSession: true,
+    autoRefreshToken: true,
+    // cookieOptions: {
+    //     maxAge: 1000 * 60 * 60 * 24 * 7,
+    //     sameSite: 'strict',
+    //     secure: true,
+    //     httpOnly: true
+    // },
+}
+
 // Create a single supabase client for interacting with your database
-export const supabase = createClient(config.API_URL, config.API_KEY);
+export const supabase = createClient(config.API_URL, config.API_KEY, options);
 
 export default supabase;
