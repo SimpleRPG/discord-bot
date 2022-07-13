f38c884f2764a8185a9687b47ea57a9317102c6b
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-13 09:59:31 +0000

    Format

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,89 +1,82 @@
-import { AttributeEnum, LocationEnum } from "../enums";
-import supabase from "../supabase";
-import type { definitions } from "../types/supabase";
+import { AttributeEnum, LocationEnum } from '../enums';
+import supabase from '../supabase';
+import type { definitions } from '../types/supabase';
 
 export const registerUser = async (discordId: string): Promise<boolean> => {
-    // check if character with discord id matches a character in the database
-    if (await checkCharacterExists(discordId)) {
-        return false;
-    }
+	// check if character with discord id matches a character in the database
+	if (await checkCharacterExists(discordId)) {
+		return false;
+	}
 
-    let { body: character } = await supabase
-        .from<definitions['characters']>('characters')
-        .insert({
-            discord_id: discordId,
-            level: 1,
-            exp: 0,
-            money: 200,
-            location_id: LocationEnum.Hometown,
-        })
-        .single();
+	let { body: character } = await supabase
+		.from<definitions['characters']>('characters')
+		.insert({
+			discord_id: discordId,
+			level: 1,
+			exp: 0,
+			money: 200,
+			location_id: LocationEnum.Hometown
+		})
+		.single();
 
-    await supabase.from<definitions['character_attributes']>('character_attributes')
-        .insert([
-            {
-                attribute_id: AttributeEnum.HP,
-                character_id: character!.id,
-                value: 50,
-            },
-            {
-                attribute_id: AttributeEnum.Strength,
-                character_id: character!.id,
-                value: 15,
-            },
-            {
-                attribute_id: AttributeEnum.Defense,
-                character_id: character!.id,
-                value: 5,
-            },
-            {
-                attribute_id: AttributeEnum.CriticalChance,
-                character_id: character!.id,
-                value: 0,
-            },
-            {
-                attribute_id: AttributeEnum.CriticalDamage,
-                character_id: character!.id,
-                value: 0,
-            },
-            {
-                attribute_id: AttributeEnum.EvadeChance,
-                character_id: character!.id,
-                value: 0,
-            },
-            {
-                attribute_id: AttributeEnum.EscapeChance,
-                character_id: character!.id,
-                value: 0,
-            },
-        ]);
+	await supabase.from<definitions['character_attributes']>('character_attributes').insert([
+		{
+			attribute_id: AttributeEnum.HP,
+			character_id: character!.id,
+			value: 50
+		},
+		{
+			attribute_id: AttributeEnum.Strength,
+			character_id: character!.id,
+			value: 15
+		},
+		{
+			attribute_id: AttributeEnum.Defense,
+			character_id: character!.id,
+			value: 5
+		},
+		{
+			attribute_id: AttributeEnum.CriticalChance,
+			character_id: character!.id,
+			value: 0
+		},
+		{
+			attribute_id: AttributeEnum.CriticalDamage,
+			character_id: character!.id,
+			value: 0
+		},
+		{
+			attribute_id: AttributeEnum.EvadeChance,
+			character_id: character!.id,
+			value: 0
+		},
+		{
+			attribute_id: AttributeEnum.EscapeChance,
+			character_id: character!.id,
+			value: 0
+		}
+	]);
 
-    return true;
-}
+	return true;
+};
 
 export const getCharacterCurrentLocation = async (character: definitions['characters']): Promise<definitions['locations']> => {
-    let { body: locations } = await supabase
-        .from<definitions['locations']>('locations')
-        .select()
-        .eq('id', character.location_id);
+	let { body: locations } = await supabase.from<definitions['locations']>('locations').select().eq('id', character.location_id);
 
-    return locations![0];
-}
+	return locations![0];
+};
 
 export const checkCharacterExists = async (discordId: string) => {
-    const res = await supabase
-        .from<definitions['characters']>('characters')
-        .select('discord_id')
-        .eq('discord_id', discordId);
-    if (res.body![0]) {
-        return true;
-    }
-    return false;
-}
+	const res = await supabase.from<definitions['characters']>('characters').select('discord_id').eq('discord_id', discordId);
+	if (res.body![0]) {
+		return true;
+	}
+	return false;
+};
 
 export const attributeValueToString = (value: number, isPercentage: boolean): string => {
-    if (isPercentage) {
-        return `${value * 100}%`;
-    }
-    return `${value}`;
-}
+	if (isPercentage) {
+		return `${value * 100}%`;
+	}
+	return `${value}`;
+};
--- src/supabase.ts
+++ src/supabase.ts
@@ -1,16 +1,16 @@
-import { createClient, SupabaseClientOptions } from '@supabase/supabase-js'
+import { createClient, SupabaseClientOptions } from '@supabase/supabase-js';
 import config from './config';
 
 const options = <SupabaseClientOptions>{
-    persistSession: true,
-    autoRefreshToken: true,
-    // cookieOptions: {
-    //     maxAge: 1000 * 60 * 60 * 24 * 7,
-    //     sameSite: 'strict',
-    //     secure: true,
-    //     httpOnly: true
-    // },
-}
+	persistSession: true,
+	autoRefreshToken: true
+	// cookieOptions: {
+	//     maxAge: 1000 * 60 * 60 * 24 * 7,
+	//     sameSite: 'strict',
+	//     secure: true,
+	//     httpOnly: true
+	// },
+};
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(config.API_URL, config.API_KEY, options);
