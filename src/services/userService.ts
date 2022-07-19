ab5d3538351c9a8f045a970d44324b1b6b93a563
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-19 15:29:21 +0000

    Remove supabase implementations

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,74 +1,54 @@
+import { prisma } from "../db";
 import { LocationEnum } from "../enums";
-import supabase from "../supabase";
-import type { definitions } from "../types/supabase";
-
-const defaultCharacterValues = <definitions["characters"]>{
-    level: 1,
-    exp: 0,
-    money: 200,
-    location_id: LocationEnum.Hometown,
-};
-
-const getDefaultCharacterAttributeValues = async (characterId: number) => {
-    const { data: attributes } = await supabase
-        .from<definitions["attributes"]>("attributes")
-        .select()
-        .order("id");
-
-    return (
-        attributes?.map((attribute) => ({
-            character_id: characterId,
-            attribute_id: attribute.id,
-            value: attribute.character_default_value,
-        })) || []
-    );
-};
 
 export const registerUser = async (discordId: string): Promise<boolean> => {
     // check if character with discord id matches a character in the database
-    if (await checkCharacterExists(discordId)) {
+    if (await characterExists(discordId)) {
         return false;
     }
 
-    const { body: character } = await supabase
-        .from<definitions["characters"]>("characters")
-        .insert({
-            ...defaultCharacterValues,
+    const character = await prisma.characters.create({
+        data: {
+            level: 1,
+            exp: 0,
+            money: 200,
+            location_id: LocationEnum.Hometown,
             discord_id: discordId,
-        })
-        .single();
+        },
+    });
 
     if (character === null) {
         return false;
     }
 
-    await supabase
-        .from<definitions["character_attributes"]>("character_attributes")
-        .insert(await getDefaultCharacterAttributeValues(character.id));
+    // set default attribute values
+    const attributes = await prisma.attributes.findMany();
+
+    if (attributes === null) {
+        return false;
+    }
+
+    await prisma.character_attributes.createMany({
+        data: attributes.map((attribute) => {
+            return {
+                character_id: character.id,
+                attribute_id: attribute.id,
+                value: attribute.character_default_value,
+            };
+        }),
+    });
 
     return true;
 };
 
-export const getCharacterCurrentLocation = async (
-    character: definitions["characters"]
-): Promise<definitions["locations"]> => {
-    const { body: locations } = await supabase
-        .from<definitions["locations"]>("locations")
-        .select()
-        .eq("id", character.location_id);
+export const characterExists = async (discordId: string) => {
+    const characterCount = await prisma.characters.count({
+        where: {
+            discord_id: discordId,
+        },
+    });
 
-    return locations![0];
-};
-
-export const checkCharacterExists = async (discordId: string) => {
-    const res = await supabase
-        .from<definitions["characters"]>("characters")
-        .select("discord_id")
-        .eq("discord_id", discordId);
-    if (res.body![0]) {
-        return true;
-    }
-    return false;
+    return characterCount !== 0;
 };
 
 export const attributeValueToString = (
